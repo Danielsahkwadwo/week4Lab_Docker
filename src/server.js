a34875bed1f92e422c31cb17a8f76fc4211cbf98
danielsahkwadwo <daniel.sah@amalitech.com>
2025-03-05 14:09:10 +0000

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Serve the main HTML page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
