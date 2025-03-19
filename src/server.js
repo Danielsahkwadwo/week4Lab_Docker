f39298375451b93ef97effd05aa363b5fb405847
danielsahkwadwo <daniel.sah@amalitech.com>
2025-03-19 15:38:29 +0000

    modified server file

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 8080;
 // Serve static files (CSS, images, etc.)
 app.use(express.static(path.join(__dirname, "public")));
 
+
+
 // Serve the main HTML page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
